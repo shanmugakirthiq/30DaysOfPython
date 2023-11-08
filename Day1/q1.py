215dc31bed87e978579c7ad4b5622320a6950cb7
shanmugakirthiq <81150912+shanmugakirthiq@users.noreply.github.com>
2023-11-08 23:09:59 +0530

    Day 1 Programming Questions

---
+++ Day1/q1.py
@@ -0,0 +1,5 @@
+x = int(input("Enter a positive integer x: "))
+y = int(input("Enter a positive integer y: "))
+result = x ** y
+num_digits = len(str(result))
+print(" ",num_digits)
---
+++ Day1/q2.py
@@ -0,0 +1,9 @@
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+word3 = input("Enter the third word: ")
+word4 = input("Enter the fourth word: ")
+word5 = input("Enter the fifth word: ")
+
+sentence = word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + "."
+
+print("The sentence is:", sentence)
